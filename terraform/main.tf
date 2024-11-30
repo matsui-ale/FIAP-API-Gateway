852ed42d5c57e2257275061cf847d2212fd8ef92
matsui-ale <matsui-alexandre@outlook.com>
2024-11-30 14:11:00 -0300

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,7 +121,7 @@ resource "aws_api_gateway_resource" "produto_categoria_resource" {
 }
 
 # Methods and Integrations for Each Endpoint
-\
+\*
 ### /Cliente/{cpf} - GET ###
 resource "aws_api_gateway_method" "get_cliente_by_cpf" {
   rest_api_id   = aws_api_gateway_rest_api.lanchonete_api.id
@@ -133,6 +133,7 @@ resource "aws_api_gateway_method" "get_cliente_by_cpf" {
     "method.request.path.cpf" = true
   }
 }
+*/
 
 /*
 resource "aws_api_gateway_integration" "get_cliente_by_cpf_integration" {
